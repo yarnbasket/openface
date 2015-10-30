dbae0addad6333f52e09a7eafe4898c7355d4c92
Phillip Schanely <phil@schanely.com>
2015-10-29 22:09:45 -0400

    minimal setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='openface',
+      version='0.1',
+      description="Face recognition with Google's FaceNet deep neural network.",
+      url='https://github.com/yarnbasket/openface',
+      packages=['openface','openface.alignment'],
+  )
